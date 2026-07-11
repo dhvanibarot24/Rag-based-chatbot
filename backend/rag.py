000973475ea9f659a272043bac75fac72c65a6e5
dhvanibarot24 <dhvanibarot24@gmail.com>
2026-07-11 16:51:33 +0530

    integrated groq

--- backend/rag.py
+++ backend/rag.py
@@ -1,20 +1,53 @@
 from sentence_transformers import SentenceTransformer
+from groq import Groq
+from dotenv import load_dotenv
 import chromadb
+import os
+
+load_dotenv()
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 db = chromadb.PersistentClient(path="chroma_db")
-
 col = db.get_collection("company")
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 
 def search(question):
 
-    e = model.encode(question).tolist()
+    embedding = model.encode(question).tolist()
 
     result = col.query(
-        query_embeddings=[e],
-        n_results=3
+        query_embeddings=[embedding],
+        n_results=1
     )
 
-    return "\n\n".join(result["documents"][0])
+    context = result["documents"][0][0]
+
+    prompt = f"""
+You are an AI assistant for Scaller Technologies.
+
+Answer the question only using the information given below.
+
+If the answer is not available, reply:
+I don't have that information.
+
+Information:
+{context}
+
+Question:
+{question}
+"""
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
