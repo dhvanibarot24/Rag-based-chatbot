eab48b7427c8295accbf0b71fd308531250a4a15
dhvanibarot24 <dhvanibarot24@gmail.com>
2026-07-11 16:05:24 +0530

    completed UI

--- frontend/UI.py
+++ frontend/UI.py
@@ -3,33 +3,66 @@ import requests
 
 st.set_page_config(
     page_title="Scaller AI Assistant",
-    page_icon="🤖"
+    page_icon="🤖",
+    layout="centered"
 )
 
-st.title("🤖 Scaller Technologies AI Assistant")
+st.markdown("""
+<style>
 
-st.write("Ask anything about Scaller Technologies.")
+.stApp{
+    background: linear-gradient(135deg,#EEF6FF,#D9EFFF);
+}
 
-q = st.text_input("Enter your question")
+h1{
+    text-align:center;
+    color:#1E3A8A;
+}
+
+p{
+    text-align:center;
+    font-size:18px;
+}
+
+div.stButton > button{
+    width:100%;
+    height:50px;
+    border-radius:12px;
+    font-size:18px;
+    font-weight:bold;
+    background-color:#2563EB;
+    color:white;
+}
+
+
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("<h1>🤖 Scaller Technologies AI Assistant</h1>", unsafe_allow_html=True)
+
+st.markdown(
+    "<p>Ask anything about Scaller Technologies</p>",
+    unsafe_allow_html=True
+)
+
+question = st.text_input("Enter your question")
 
 if st.button("Ask"):
 
-    if q == "":
+    if question == "":
         st.warning("Please enter a question.")
 
     else:
 
         with st.spinner("Generating answer..."):
 
-            r = requests.post(
+            response = requests.post(
                 "http://127.0.0.1:8000/chat",
-                json={"question": q}
+                json={"question": question}
             )
 
-            a = r.json()
-
-        st.success("Answer Generated")
+            answer = response.json()["answer"]
 
         st.subheader("Answer")
 
-        st.write(a["answer"])
+        st.info(answer)
