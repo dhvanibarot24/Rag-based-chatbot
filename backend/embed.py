625fb4882869fef5add905f188509972d4fdd518
dhvanibarot24 <dhvanibarot24@gmail.com>
2026-07-11 12:49:04 +0530

    store data

---
+++ backend/embed.py
@@ -0,0 +1,30 @@
+from sentence_transformers import SentenceTransformer
+import chromadb
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+db = chromadb.PersistentClient(path="chroma_db")
+
+try:
+    db.delete_collection("company")
+except:
+    pass
+
+col = db.get_or_create_collection("company")
+
+file = open("data.txt", "r", encoding="utf-8")
+text = file.read()
+file.close()
+
+data = text.split("\n\n")
+
+for i in range(len(data)):
+    e = model.encode(data[i]).tolist()
+
+    col.add(
+        ids=[str(i)],
+        documents=[data[i]],
+        embeddings=[e]
+    )
+
+print("Data added successfully")
